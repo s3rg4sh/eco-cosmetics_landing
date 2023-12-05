8e1177c4fe18a3e55a4497ff5ca1a2dc1536647c
Serhii <gashysh@protonmail.com>
2023-12-05 15:59:45 +0200

    minor bugs fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,12 @@
 const form = document.querySelector('[name="contact-form"]');
 
 window.addEventListener('hashchange', () => {
+  const vw = window.innerWidth;
+
   if (
-    window.location.hash === '#menu'
-    || window.location.hash === '#all-products'
+    (window.location.hash === '#menu'
+      || window.location.hash === '#all-products')
+    && vw < 1440
   ) {
     document.body.classList.add('page__body--with-menu');
   } else {
